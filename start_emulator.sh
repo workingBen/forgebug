f24955e52a0776ce9d5a026d97fb37b40dd8040f
Ben Pellow <benjmain.pellow@gmail.com>
2012-04-13 17:24:18 -0700

    fix 'is emulator started' conditional

--- start_emulator.sh
+++ start_emulator.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-if [ `ps -ax | grep froyo | grep emulator-arm` ] 
-then
-  return false
-else
+if [[ `ps -ax | grep froyo | grep emulator-arm` == "" ]]; then 
   emulator -avd froyo -scale 0.7 &
+else
+  false
 fi;
